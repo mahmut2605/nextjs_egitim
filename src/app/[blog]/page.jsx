0f4297c9fe0c7eac27d651dd3fa0a935965df6aa
mahmutcelik <mahmutcelik2605@gmail.com>
2024-12-01 21:16:32 +0300

    static routes ve dynamic routes

---
+++ src/app/[blog]/page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Blog ({params}){
+    return <div>Blog sayfası :{params.blog} </div>
+}
---
+++ src/app/hakkimizda/page.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Hakkimizda() {
+    return <div>Hakkımızda Sayfası</div>;
+}
